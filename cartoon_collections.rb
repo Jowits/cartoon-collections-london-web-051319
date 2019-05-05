ff14b1afd5cd044df267abe7060a273b8f088dae
Jowita Lapies <jowita.lapies@gmail.com>
2019-05-05 17:34:13 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,21 +1,36 @@
-def roll_call_dwarves(value)# code an argument here
+dwarves = %w[
+  Doc
+  Dopey
+  Bashful
+  Grumpy
+  Sneezy
+  Sleepy
+  Happy
+]
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
 
-  ["Doc", "Dopey", "Bashful", "Grumpy"].each.with_index(1) do |value, index|
-    puts "#{index} #{value}"
-end
+  dwarves.each_with_index do |value, index|
+    puts "#{index + 1}. #{value}"
   end
 end
 
-def summon_captain_planet# code an argument here
+planeteer_calls = %w[earth wind fire water heart]
+def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
+  planeteer_calls.map { |call| call.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
+
+def long_planeteer_calls(planeteer_calls)# code an argument here
   # Your code here
+  planeteer_calls.any? { |word| word.length > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(potentially_cheesy_items)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  potentially_cheesy_items.find do |maybe_cheese|
+    cheese_types.include?(maybe_cheese)
+  end
 end
