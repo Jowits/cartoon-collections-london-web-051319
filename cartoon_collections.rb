bc9e2aa6cc808d27d5eee7fdaa9f41d5672741f8
Jowita Lapies <jowita.lapies@gmail.com>
2019-05-15 07:35:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,9 @@ dwarves = %w[
 ]
 def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
-
+  dwarves.each_with_index(1)do |value, index|
+  puts "#{index + 1}: #{value}"
+end
 
 
 def summon_captain_planet(planeteer_calls)# code an argument here
@@ -24,5 +26,5 @@ end
 
 def find_the_cheese(potentially_cheesy_items)# code an argument here
   # the array below is here to help
-  
+
 end
